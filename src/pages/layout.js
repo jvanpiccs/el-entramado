033908f9c813971c9b3da47b16b7e465863e8a50
jvanpiccs <9990398+jvanpiccs@users.noreply.github.com>
2022-02-06 23:03:48 -0300

    nav reemplazado

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Container, Menu } from 'semantic-ui-react';
 
 function Layout(props) {
+    let [active, setActive] = React.useState();
+    {/* <nav className='Nav'>
+        <Link to='/'>Entramado</Link>
+        <Link to='/'>Inicio</Link>
+        <Link to='/about'>Quienes somos</Link>
+    </nav> */}
+    const links = [
+        {
+            name: 'Inicio',
+            link: '/'
+        }, {
+            name: 'Quienes somos',
+            link: '/about'
+        }
+    ]
     return (
-        <nav className='Nav'>
-            <Link to='/'>Entramado</Link>
-            <Link to='/'>Inicio</Link>
-            <Link to='/about'>Quienes somos</Link>
-        </nav>
+        <Menu className={'Nav'} fixed='top' inverted stackable borderless>
+            <Container>
+                <Menu.Header><Link to='/'>Entramado Social</Link></Menu.Header>
+                <Menu.Item><Link to='/'>Inicio</Link></Menu.Item>
+                <Menu.Item><Link to='/about'>Nosotros</Link></Menu.Item>
+            </Container>
+        </Menu>
     );
 };
 
